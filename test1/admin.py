0135e504693e8b538f9483a8c74a4cdd15f1051a
aliyar <aliyrabdrasitov112@gmail.com>
2022-12-27 22:23:57 +0600

    fix bag

--- test1/admin.py
+++ test1/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from test1.models import Category, Post
+from test1.models import Category, Post, Comment
 
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Comment)
--- test1/models.py
+++ test1/models.py
@@ -2,11 +2,20 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=40,unique=True)
-
+    def __self__(self):
+        return Category
 class Post(models.Model):
     title = models.CharField(max_length=80)
     text = models.TextField()
-    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     reated_at = models.DateTimeField(
         auto_now_add=True)
+    def __self__(self):
+        return Post
+class Comment(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    comment = models.ForeignKey(Post, on_delete=models.CASCADE)
+    def __self__(self):
+        return Comment
 
